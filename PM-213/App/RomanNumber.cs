07250460798343ad91f230519a79ef8ac1ebc1fd
Landic <danil.volkov.235@gmail.com>
2024-09-10 14:22:49 +0300

    hw done

--- PM-213/App/RomanNumber.cs
+++ PM-213/App/RomanNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -15,60 +16,55 @@ namespace App
 
         public static RomanNumber Parse(string input)
         {
-            //input = input.ToUpper();
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new FormatException($"{nameof(RomanNumber)}.{nameof(Parse)}: Input string is null or empty");
+            }
 
-            //if (string.IsNullOrEmpty(input))
-            //{
-            //    return new RomanNumber(0);
-            //}
-
-            //Dictionary<char,int> romanMap = new Dictionary<char, int>
-            //{
-            //    {'I', 1},
-            //    {'V', 5},
-            //    {'X', 10},
-            //    {'L', 50},
-            //    {'C', 100},
-            //    {'D', 500},
-            //    {'M', 1000}
-            //};
-
-            //if (romanMap.TryGetValue(input[0], out int value))
-            //{
-            //    return new RomanNumber(value);
-            //}
-            //else
-            //{
-            //    throw new ArgumentException("Invalid Roman number!");
-            //}
             int value = 0;
             int prevDigit = 0;
             int pos = input.Length;
-            List<String> errors = new();
+            int maxDigit = 0;
+            bool hasLesserDigit = false;
+
             foreach (char c in input.Reverse())
             {
                 pos -= 1;
                 int digit;
+
                 try
                 {
                     digit = DigitalValue(c.ToString());
                 }
                 catch
                 {
-                    throw new FormatException($"Invalid symbol '{c}' in position {pos}");
+                    throw new FormatException($"{nameof(RomanNumber)}.{nameof(Parse)}: Invalid symbol '{c}' in position {pos}");
                 }
 
                 if (digit != 0 && prevDigit / digit > 10)
                 {
-                    throw new FormatException($"Invalid order '{c}' before '{input[pos + 1]}' in position {pos}");
+                    throw new FormatException($"{nameof(RomanNumber)}.{nameof(Parse)}: Invalid order '{c}' before '{input[pos + 1]}' in position {pos}");
                 }
 
-                if (prevDigit > digit &&
-                    !((digit == 1 && (prevDigit == 5 || prevDigit == 10)) ||
-                     (digit == 10 && (prevDigit == 50 || prevDigit == 100)) ||
-                     (digit == 100 && (prevDigit == 500 || prevDigit == 1000))))
+                if (digit < maxDigit)
                 {
-                    throw new FormatException($"Invalid combination of '{c}' in position {pos}");
+                    if (hasLesserDigit)
+                    {
+                        throw new FormatException($"{nameof(RomanNumber)}.{nameof(Parse)}: invalid sequence: more than 1 less digit before '{input[^1]}'");
+                    }
+                    hasLesserDigit = true;
+                }
+                else
+                {
+                    maxDigit = digit;
+                    hasLesserDigit = false;
+                }
+
+                if (prevDigit > digit && !((digit == 1 && (prevDigit == 5 || prevDigit == 10)) ||
+                                           (digit == 10 && (prevDigit == 50 || prevDigit == 100)) ||
+                                           (digit == 100 && (prevDigit == 500 || prevDigit == 1000))))
+                {
+                    throw new FormatException($"{nameof(RomanNumber)}.{nameof(Parse)}: Invalid combination of '{c}' in position {pos}");
                 }
 
                 value += digit >= prevDigit ? digit : -digit;
@@ -77,6 +73,10 @@ namespace App
 
             return new RomanNumber(value);
         }
+
+
+
+
 
         public static int DigitalValue(String digit) => digit switch
         {
--- PM-213/Test/RomanNumberTest.cs
+++ PM-213/Test/RomanNumberTest.cs
@@ -69,109 +69,129 @@ namespace Test
         [TestMethod]
         public void ParseTest()
         {
-            Dictionary<String, int> romanMap = new()
-            {
-                {"N", 0},
-                {"I", 1},
-                {"II", 2},
-                {"III", 3},
-                {"IIII", 4},
-                {"IV", 4},
-                {"V", 5},
-                {"VI", 6},
-                {"VII", 7},
-                {"VIII", 8},
-                {"VIIII", 9},
-                {"IX", 9},
-                {"X", 10},
-                {"XI", 11},
-                {"XII", 12},
-                {"XIII", 13},
-                {"XIIII", 14},
-                {"XIV", 14},
-                {"XV", 15},
-                {"XVI", 16},
-                {"XX", 20},
-                {"XXX", 30},
-                {"XL", 40},
-                {"XXXX", 40},
-                {"L", 50},
-                {"LX", 60},
-                {"LXXXX", 90},
-                {"XC", 90},
-                {"C", 100},
-                {"CC", 200},
-                {"CCC", 300},
-                {"CD", 400},
-                {"D", 500},
-                {"DC", 600},
-                {"DCCC", 800},
-                {"CM", 900},
-                {"M", 1000},
-                {"MC", 1100},
-                {"MCM", 1900},
-                {"MM", 2000},
-                {"MMM", 3000},
-                {"MMMM", 4000}
-            };
-            foreach (var test in romanMap)
-            {
-                RomanNumber rn = RomanNumber.Parse(test.Key);
-                Assert.IsNotNull(rn);
-                Assert.AreEqual(test.Value, rn.Value, $"{test.Key} -> {test.Value}");
-            }
+            //Dictionary<String, int> romanMap = new()
+            //{
+            //    {"N", 0},
+            //    {"I", 1},
+            //    {"II", 2},
+            //    {"III", 3},
+            //    {"IIII", 4},
+            //    {"IV", 4},
+            //    {"V", 5},
+            //    {"VI", 6},
+            //    {"VII", 7},
+            //    {"VIII", 8},
+            //    {"VIIII", 9},
+            //    {"IX", 9},
+            //    {"X", 10},
+            //    {"XI", 11},
+            //    {"XII", 12},
+            //    {"XIII", 13},
+            //    {"XIIII", 14},
+            //    {"XIV", 14},
+            //    {"XV", 15},
+            //    {"XVI", 16},
+            //    {"XX", 20},
+            //    {"XXX", 30},
+            //    {"XL", 40},
+            //    {"XXXX", 40},
+            //    {"L", 50},
+            //    {"LX", 60},
+            //    {"LXXXX", 90},
+            //    {"XC", 90},
+            //    {"C", 100},
+            //    {"CC", 200},
+            //    {"CCC", 300},
+            //    {"CD", 400},
+            //    {"D", 500},
+            //    {"DC", 600},
+            //    {"DCCC", 800},
+            //    {"CM", 900},
+            //    {"M", 1000},
+            //    {"MC", 1100},
+            //    {"MCM", 1900},
+            //    {"MM", 2000},
+            //    {"MMM", 3000},
+            //    {"MMMM", 4000}
+            //};
+            //foreach (var test in romanMap)
+            //{
+            //    RomanNumber rn = RomanNumber.Parse(test.Key);
+            //    Assert.IsNotNull(rn);
+            //    Assert.AreEqual(test.Value, rn.Value, $"{test.Key} -> {test.Value}");
+            //}
 
 
-            Dictionary<string, (char, int)[]> exTestCases = new()
+            //Dictionary<string, (char, int)[]> exTestCases = new()
+            //{
+            //    {"W", new[] {('W', 0)}},
+            //    {"Q", new[] {('Q', 0)}},
+            //    {"s", new[] {('s', 0)}},
+            //    {"Xd", new[] {('d', 1)}},
+            //    {"SWXF", new[] { ('S', 0), ('W', 1), ('F', 3) }},
+            //    {"AIXL", new[] { ('A', 0) }},
+            //    {"MMQ", new[] { ('Q', 2) }},
+            //    {"XDDX", new[] { ('D', 1), ('D', 2) }},
+            //    {"YIYV", new[] { ('Y', 0), ('Y', 2) }},
+            //};
+            //foreach (var testCase in exTestCases)
+            //{
+            //    var ex = Assert.ThrowsException<FormatException>(
+            //        () => RomanNumber.Parse(testCase.Key),
+            //        $"{nameof(FormatException)} Parse '{testCase.Key}' must throw"
+            //    );
+            //    foreach (var (symbol, position) in testCase.Value)
+            //    {
+            //        Assert.IsTrue(ex.Message.Contains($"Invalid symbol '{symbol}' in position {position}"),
+            //            $"{nameof(FormatException)} must contain data about symbol '{symbol}' at position {position}. " +
+            //            $"TestCase: '{testCase.Key}', ex.Message: '{ex.Message}'"
+            //        );
+            //    }
+            //}
+            //Dictionary<String, Object[]> invalidOrderTestCases = new()
+            //{
+            //    { "IM",  ['I', 'M', 0] },
+            //    { "XIM", ['I', 'M', 1] },
+            //    { "IMX", ['I', 'M', 0] },
+            //    { "XMD", ['X', 'M', 0] },
+            //    { "XID", ['I', 'D', 1] },
+            //    { "ID", ['I', 'D', 0] },
+            //    { "VX", ['V', 'X', 0] },
+            //    { "LC", ['L', 'C', 0] },
+            //    { "VV", ['V', 'V', 0] },
+            //    { "LL", ['L', 'L', 0] }
+            //};
+
+            //foreach (var testCase in invalidOrderTestCases)
+            //{
+            //    var ex = Assert.ThrowsException<FormatException>(
+            //        () => RomanNumber.Parse(testCase.Key),
+            //        $"{nameof(FormatException)} Parse '{testCase.Key}' must throw"
+            //    );
+            //    Assert.IsTrue(
+            //        ex.Message.Contains($"Invalid order '{testCase.Value[0]}' before '{testCase.Value[1]}' in position {testCase.Value[2]}"),
+            //        $"FormatException must contain data about mis-ordered symbols. TestCase: '{testCase.Key}', ex.Message: '{ex.Message}'"
+            //    );
+            //}
+
+            String[] exTestCases3 =
             {
-                {"W", new[] {('W', 0)}},
-                {"Q", new[] {('Q', 0)}},
-                {"s", new[] {('s', 0)}},
-                {"Xd", new[] {('d', 1)}},
-                {"SWXF", new[] { ('S', 0), ('W', 1), ('F', 3) }},
-                {"AIXL", new[] { ('A', 0) }},
-                {"MMQ", new[] { ('Q', 2) }},
-                {"XDDX", new[] { ('D', 1), ('D', 2) }},
-                {"YIYV", new[] { ('Y', 0), ('Y', 2) }},
+                "IXC", "IIX", "VIX",
+                "IIXC", "IIIX", "VIIX",
+                "VIXC", "IVIX", "CVIIX",
+                "IXCC", "IXCM", "IXXC",
             };
-            foreach (var testCase in exTestCases)
+
+            foreach (var testCase in exTestCases3)
             {
                 var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(testCase.Key),
-                    $"{nameof(FormatException)} Parse '{testCase.Key}' must throw"
+                   () => RomanNumber.Parse(testCase),
+                    $"{nameof(FormatException)} Parse '{testCase}' must throw"
                 );
-                foreach (var (symbol, position) in testCase.Value)
-                {
-                    Assert.IsTrue(ex.Message.Contains($"Invalid symbol '{symbol}' in position {position}"),
-                        $"{nameof(FormatException)} must contain data about symbol '{symbol}' at position {position}. " +
-                        $"TestCase: '{testCase.Key}', ex.Message: '{ex.Message}'"
-                    );
-                }
-            }
-            Dictionary<String, Object[]> invalidOrderTestCases = new()
-            {
-                { "IM",  ['I', 'M', 0] },
-                { "XIM", ['I', 'M', 1] },
-                { "IMX", ['I', 'M', 0] },
-                { "XMD", ['X', 'M', 0] },
-                { "XID", ['I', 'D', 1] },
-                { "ID", ['I', 'D', 0] },
-                { "VX", ['V', 'X', 0] },
-                { "LC", ['L', 'C', 0] },
-                { "VV", ['V', 'V', 0] },
-                { "LL", ['L', 'L', 0] }
-            };
-
-            foreach (var testCase in invalidOrderTestCases)
-            {
-                var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(testCase.Key),
-                    $"{nameof(FormatException)} Parse '{testCase.Key}' must throw"
-                );
-                Assert.IsTrue(
-                    ex.Message.Contains($"Invalid order '{testCase.Value[0]}' before '{testCase.Value[1]}' in position {testCase.Value[2]}"),
-                    $"FormatException must contain data about mis-ordered symbols. TestCase: '{testCase.Key}', ex.Message: '{ex.Message}'"
-                );
+                Assert.IsTrue(ex.Message.Contains(nameof(RomanNumber)) &&
+                              ex.Message.Contains(nameof(RomanNumber.Parse)) &&
+                              ex.Message.Contains($"invalid sequence: more than 1 less digit before '{testCase[^1]}'"),
+                              $"ex.Message must contain info about origin, cause and data. {ex.Message}");
             }
 
         }
